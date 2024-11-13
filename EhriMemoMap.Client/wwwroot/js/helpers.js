fe152194c6db75f7c2de187453e87d6f9cf83255
Jan Hron <hronjan@gmail.com>
2024-11-13 15:39:30 +0100

    Refactor gallery and update API URLs
    - Moved hidden image elements inside each document card in `DocumentGallery.razor`.
    - Updated `ShowImage` method to initialize gallery with specific document index.
    - Included `viewer.js` in publish directory and excluded it from single file publishing in `EhriMemoMap.Client.csproj`.
    - Changed `ApiURL` in `appsettings.json` to production API URL.
    - Reformatted `viewer.css` for better readability.
    - Updated `index.html` to reference `viewer.js` instead of `viewer.min.js`.
    - Initialized gallery with specific document ID and set default view to first image in `helpers.js`.
    - Changed `url` fields to production API URLs in `mapsettings.pacov.json` and `mapsettings.prague.json`.

--- EhriMemoMap.Client/wwwroot/js/helpers.js
+++ EhriMemoMap.Client/wwwroot/js/helpers.js
@@ -1,27 +1,25 @@
 ﻿var gallery;
 
-function initGallery() {
+function initGallery(id) {
     if (gallery !== undefined) {
         gallery.destroy();
     }
 
-    var galleryElement = document.getElementById('documents');
+    var galleryElement = document.getElementById('documents_' + id);
     if (galleryElement == null)
         return;
+
+    var imageCount = galleryElement.getElementsByTagName('img').length;
 
     gallery = new Viewer(galleryElement, {
         transition: false,
         toolbar: false,
+        navbar: imageCount > 1,
         title: [1, function(image, imageData) {
             return image.alt.replace("image ", "");
         }]
         
     });
-}
 
-function showImage(imageNumber) {
-    if (gallery == undefined)
-        return;
-    console.log(imageNumber);
-    gallery.view(imageNumber);
+    gallery.view();
 }
